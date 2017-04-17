29f94f55a00b28e7d73a25616fd424b7042ec0e1
Chris <cbrown605@googlemail.com>
2017-04-17 08:12:22 +0100

    can get abv of beer

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ var app = function(){
 
 var beerList = new BeerList('https://api.punkapi.com/v2/beers')
 var beerSelectView = new BeerSelectView(document.querySelector('#beers'))
+var beerDetailsView = new BeerDetailsView(document.querySelector('#beer-details'), document.querySelector('#clear-details'))
 
 beerList.getData(function(beers){
   beerSelectView.render(beers)
--- public/beerDetailsView.js
+++ public/beerDetailsView.js
@@ -1,14 +1,15 @@
 var BeerDetailsView = function(detailsElement, clearButton){
   this.detailsElement = detailsElement
-  this.clearButton - clearButton
+  this.clearButton = clearButton
   this.clearButton.addEventListener('click', this.clear.bind(this))
 
 }
 
 BeerDetailsView.prototype = {
+
 render: function(beer){
   var pTag = document.createElement('p')
-  pTag.innerText = beer.abv
+  pTag.innerText = 'the ABV of this beer is '+beer.abv + ' %'
   this.detailsElement.appendChild(pTag)
 },
 
